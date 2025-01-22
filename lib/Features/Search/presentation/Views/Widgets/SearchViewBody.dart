bb23e2bffa2aed8ee8c310e81e5763309ebb338a
Ahmedkhedr2 <ahmedkhedrx1@gmail.com>
2025-01-22 11:38:15 +0200

    ui changes

--- lib/Features/Search/presentation/Views/Widgets/SearchViewBody.dart
+++ lib/Features/Search/presentation/Views/Widgets/SearchViewBody.dart
@@ -1,36 +1,53 @@
+import 'package:bookly_app/Core/utils/AppRouter.dart';
 import 'package:bookly_app/Core/utils/Styless.dart';
 import 'package:bookly_app/Features/Search/presentation/Views/Widgets/CustomSearchtextField.dart';
 import 'package:bookly_app/Features/Search/presentation/Views/Widgets/SearchResultListView.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class Searchviewbody extends StatelessWidget {
   const Searchviewbody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16),
+    return SafeArea(
       child: Column(
         children: [
-          Padding(
-            padding: EdgeInsets.only(
-              top: 60,
-            ),
-            child: CustomSearchtextField(),
+          const SizedBox(
+            height: 8,
           ),
-          SizedBox(
-            height: 16,
-          ),
-          Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                'Best Seller',
-                style: Styles.textStyle18,
+          Row(
+            children: [
+              const Expanded(
+                  child: Padding(
+                padding: EdgeInsets.only(left: 16),
+                child: CustomSearchtextField(),
               )),
-          SizedBox(
-            height: 16,
+              IconButton(onPressed: () {}, icon: const Icon(Icons.close))
+            ],
           ),
-          Expanded(child: SearchResultListView())
+          const Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        'Best Seller',
+                        style: Styles.textStyle18,
+                      )),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Expanded(child: SearchResultListView())
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
